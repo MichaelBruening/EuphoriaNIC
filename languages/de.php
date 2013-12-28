28f0e250ea1369b802866e3dd71bf86e2d6c5986
MichaelBruening <me@not-me.me>
2013-12-28 23:37:13 +0100

    added language file german/DE

---
+++ languages/de.php
@@ -0,0 +1,23 @@
+<?php
+
+$german = array(
+
+	'euphorianic' => "EuphoriaNIC",
+	'euphorianic:welcome' => "Willkommen",
+	'euphorianic:login' => "Login",
+	'euphorianic:register' => "Registrieren",
+	'euphorianic:hover_for_menu' => "Menu",
+	'euphorianic:navigation' => "Navigation",
+	'euphorianic:your_account' => "Account",
+	'euphorianic:your_dashboard' => "Dashboard",
+	'euphorianic:your_profile' => "Profil",
+	'euphorianic:your_inbox' => "Nachrichten",
+	'euphorianic:your_settings' => "Einstellungen",
+	'euphorianic:logout' => "Logout",
+
+	'euphorianic:bookmarks' => "Favoriten",
+	'euphorianic:fotos' => "Fotos",
+	'euphorianic:videos' => "Videos",
+);
+
+add_translation("de", $german);
